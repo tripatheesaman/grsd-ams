29cb8b359232edaa597a52ebb75398f5c39acd1d
Saman Tripathee <tripatheesaman1@gmail.com>
2026-03-23 09:56:55 +0545

    Updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,13 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   basePath: "/ams",
 
-
-  assetPrefix: isProd ? "/ams/" : undefined,
-
   trailingSlash: false,
-
 
   images: {
     unoptimized: true,
   },
-
 
   output: "standalone",
 };
